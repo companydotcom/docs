4f991688185a207ba8d9783903ed2e44a03c6e20
jacob.granberry <jacobgranberry@gmail.com>
2023-07-07 13:58:21 -0500

    ATX-2569 | adds chakra and prettier setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  $schema: 'http://json.schemastore.org/prettierrc',
+  bracketSpacing: true,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  proseWrap: 'always' // printWidth line breaks in md/mdx
+}
