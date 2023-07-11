2eaef2f9dd5b0b476407ebad2ae52434cb088536
jacob.granberry <jacobgranberry@gmail.com>
2023-07-11 11:47:01 -0500

    ATX-2569 | attempts to sync theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
-import { ColorModeScript } from "@vastly/ui";
-import { Html, Head, Main, NextScript } from "next/document";
-import { theme } from "./_app";
+import { ColorModeScript } from "@vastly/ui"
+import { Html, Head, Main, NextScript } from "next/document"
+import { theme } from "../constants/theme"
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head />
       <body>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ColorModeScript storageKey="theme" initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
